be5d6686e2e7c77188e18238aed3f3865b259aa5
Jahnavi Chennamsetty <63448333+jahnavi0105@users.noreply.github.com>
2022-01-27 10:44:56 +0530

    Create maximum_subarray.java

---
+++ maximum_subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=-10000000,s=0;
+        for(int i=0;i<nums.length;i++){
+            s+=nums[i];
+            max=Math.max(s,max);
+            if(s<0)
+                s=0;
+        }
+        return max;
+    }
+}
