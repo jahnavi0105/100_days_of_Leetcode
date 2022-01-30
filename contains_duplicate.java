30958ab18cddedee30d8171035f8f23e21a3fe10
Jahnavi Chennamsetty <63448333+jahnavi0105@users.noreply.github.com>
2022-01-30 10:53:29 +0530

    Create contains_duplicate.java

---
+++ contains_duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int c=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1])
+                c=1;
+        }
+        if(c==1)
+            return true;
+        return false;
+    }
+}
